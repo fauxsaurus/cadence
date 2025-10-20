e1f0e2caa55efedf40e8917c8cf4e78469634072
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-10-20 16:29:44 -0500

    +timer states & config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import {useEffect, useState} from 'react'
+import {defaultConfig} from './config'
+import {calculateDerivativeState, createConfig, createTimeState, setAt, type IState} from './lib'
 import './App.css'
 
-function App() {
-	const [count, setCount] = useState(0)
+const App = () => {
+	const [configs, setConfigs] = useState(defaultConfig)
+	const [timeStates, setTimeStates] = useState(configs.map(() => createTimeState()))
+
+	const [currentTime, setCurrentTime] = useState(Date.now())
+
 
 	return (
 		<>
