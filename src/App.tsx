b6538793fc366059fcf2f9b4ca49747872bfd1d7
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-10-20 16:30:32 -0500

    ensured timers update in sync

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,12 @@ const App = () => {
 
 	const [currentTime, setCurrentTime] = useState(Date.now())
 
+	useEffect(() => {
+		const interval = setInterval(() => setCurrentTime(Date.now()), 1000)
+
+		return () => clearInterval(interval)
+	})
+
 
 	return (
 		<>
